95b702658cbba4d2390bb6bf3defed07e0fdeed6
Stefan Guggisberg <sguggisb@adobe.com>
2018-06-28 17:06:08 +0200

    #129 cli skeleton (WIP)

--- src/build.js
+++ src/build.js
@@ -1,26 +1,30 @@
 /*
- *  Copyright 2018 Adobe Systems Incorporated. All rights reserved.
- *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License. You may obtain a copy
- *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under
- *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- *  OF ANY KIND, either express or implied. See the License for the specific language
- *  governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 
 'use strict';
+
+/* eslint no-console: off */
+// TODO: remove the following line
+/* eslint no-unused-vars: off */
 
 module.exports = {
   command: 'build',
   aliases: ['b'],
   desc: 'Compile the template functions and build package',
-  builder: (yargs) =>  {
-    // TODO define parameters
+  builder: (yargs) => {
+    // TODO: define parameters
   },
   handler: (argv) => {
-    // TODO implement
+    // TODO: implement
     console.log('Build', argv.name);
-  }
+  },
 };
--- src/init.js
+++ src/init.js
@@ -1,13 +1,13 @@
 /*
- *  Copyright 2018 Adobe Systems Incorporated. All rights reserved.
- *  This file is licensed to you under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License. You may obtain a copy
- *  of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under
- *  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
- *  OF ANY KIND, either express or implied. See the License for the specific language
- *  governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
  */
 
 'use strict';
@@ -20,31 +20,33 @@ const chalk = require('chalk');
 const NAME_PARAM = 'name';
 const DIR_PARAM = 'dir';
 
+/* eslint no-console: off */
+
 module.exports = {
   command: `init <${NAME_PARAM}> [${DIR_PARAM}]`,
   aliases: ['i'],
   desc: 'Initialize the project structure',
-  builder: (yargs) =>  {
+  builder: (yargs) => {
     yargs.positional(NAME_PARAM, {
       type: 'string',
-      describe: 'Name of the project to initialize'
+      describe: 'Name of the project to initialize',
     })
-    .positional(DIR_PARAM, {
-      type: 'string',
-      describe: 'Parent directory of new project',
-      default: '.'
-    })
+      .positional(DIR_PARAM, {
+        type: 'string',
+        describe: 'Parent directory of new project',
+        default: '.',
+      });
   },
   handler: (argv) => {
     const projectDir = path.resolve(path.join(argv.dir, argv.name));
     fse.ensureDir(projectDir)
-    .then(() => {
-      console.log(chalk.green(`Successfully created ${projectDir}`));
-    })
-    .catch(err => {
-      console.error(chalk.red(err)
-    });
-    // TODO implement
+      .then(() => {
+        console.log(chalk.green(`Successfully created ${projectDir}`));
+      })
+      .catch((err) => {
+        console.error(chalk.red(err));
+      });
+    // TODO: implement
     console.log(chalk.green('Init'), argv.name, argv.dir);
-  }
+  },
 };
