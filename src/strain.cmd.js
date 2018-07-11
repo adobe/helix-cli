93220c624218c4e7fc030a0fa39ffa8a244a9387
Lars Trieloff <trieloff@adobe.com>
2018-07-11 18:04:39 +0200

    WIP: publish strains

--- src/strain.cmd.js
+++ src/strain.cmd.js
@@ -12,6 +12,14 @@
 
 /* eslint no-console: off */
 
+const fs = require('fs-extra');
+const request = require('request-promise');
+const path = require('path');
+const { toBase64 } = require('request/lib/helpers');
+const strainconfig = require('./strain-config');
+const STRAIN_FILE = path.resolve(process.cwd(), '.hlx', 'strains.yaml');
+
+
 class StrainCommand {
   constructor() {
     this._wsk_auth = null;
@@ -20,6 +28,24 @@ class StrainCommand {
     this._fastly_namespace = null;
     this._fastly_auth = null;
     this._dryRun = false;
+
+    this._service = null;
+    this._options = {
+      headers: {
+        'User-Agent': 'Project Helix CLI',
+        'Accept': 'application/json'
+      },
+      json: true
+    };
+
+    this._dictionaries = {
+      secrets: null,
+      strain_action_roots: null,
+      strain_owners: null,
+      strain_refs: null,
+      strain_repos: null,
+      strain_root_paths: null
+    };
   }
 
   withWskHost(value) {
@@ -44,6 +70,7 @@ class StrainCommand {
 
   withFastlyAuth(value) {
     this._fastly_auth = value;
+    this._options.headers['Fastly-Key'] = value;
     return this;
   }
 
@@ -52,8 +79,90 @@ class StrainCommand {
     return this;
   }
 
+  options(path) {
+    return Object.assign({uri: `https://api.fastly.com/service/${this._fastly_namespace}${path}`}, this._options);
+  }
+
+  async version(path) {
+    const ver = await this.getCurrentVersion()
+    return this.options('/version/' + ver + path);
+  }
+
+  putOpts(path, value) {
+    const ver = this.options(path);
+    return Object.assign({method: 'PUT', form: {
+      item_value: value
+    }}, ver);
+  }
+
+  async getService(refresh) {
+    if (!this._service||refresh) {
+      this._service = await request(this.options(''));
+    }
+    return this._service;
+  }
+
+  async getCurrentVersion() {
+    const service = await this.getService();
+    return [...service.versions].pop().number;
+  }
+
+  async getDictionaries() {
+    // if there are undefined dictionaries, we have to reload them
+    if (Object.values(this._dictionaries).some(e => e == null)) {
+      const opts = await this.version('/dictionary');
+      const dicts = await request(opts);
+      Object.values(dicts).map(dict => {
+        if (!dict.deleted_at) {
+          this._dictionaries[dict.name] = dict.id;
+        }
+      });
+    }
+
+    return this._dictionaries;
+  }
+
+  async putDict(dict, key ,value) {
+    await this.getDictionaries();
+    const mydict =this._dictionaries[dict];
+    if (!mydict) {
+      console.log(dict + '  does not exist');
+    } else {
+      const opts = await this.putOpts('/dictionary/' + mydict + '/item/' + key, value);
+      return await request(opts);
+    }
+  }
+
   async run() {
     console.log('Publishing strains');
+
+    const owsecret = "Basic " + toBase64(this._wsk_namespace + ':' + this._wsk_auth);
+    this.putDict('secrets', 'OPENWHISK_AUTH', owsecret).then(r => {
+      console.log('🗝  Enabled Fastly to call secure OpenWhisk actions');
+    });
+
+    fs.readFile(STRAIN_FILE, (err, content) => {
+      if (!err) {
+        const strains = strainconfig.load(content);
+        strains.map(strain => {
+          this.putDict('strain_action_roots', strain.name ,strain.code).then(r => {
+            console.log('👾  Set action root for strain ' + strain.name);
+          });
+          this.putDict('strain_owners', strain.name ,strain.content.owner).then(r => {
+            console.log('🏢  Set owner for strain       ' + strain.name);
+          });
+          this.putDict('strain_repos', strain.name ,strain.content.repo).then(r => {
+            console.log('🌳  Set repo for strain        ' + strain.name);
+          });
+          if (strain.content.ref) {
+            this.putDict('strain_refs', strain.name ,strain.content.ref).then(r => {
+              console.log('🏷  Set ref for strain         ' + strain.name);
+            });
+          }       
+        });
+      }
+    })
+
     return this;
   }
 }
