9971dfa285367031f163dceb5bcb7340eb15920f
Tobias Bocanegra <tripod@adobe.com>
2018-07-25 14:39:41 +0200

    use latest server

--- test/testInitCmd.js
+++ test/testInitCmd.js
@@ -10,7 +10,7 @@
  * governing permissions and limitations under the License.
  */
 
-/* eslint-env mocha */
+/* globals describe, beforeEach, afterEach, it */
 
 const assert = require('assert');
 const path = require('path');
