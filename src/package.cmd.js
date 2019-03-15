cc92391400bd76dd765253722848824450716e7e
Lars Trieloff <trieloff@adobe.com>
2019-03-15 07:22:51 +0000

    Revert "Avoid conflicts with module-specific node_modules"
    This reverts commit 841b2fa77b7bb99389cfba89f7c288eec64e79b2.
    
    Instead, it modifies ExternalsCollector so that a new external will only be registered if,
    - it's not a known container-provided module
    - it's not already registered (preventing later modules from overriding earlier)
    - it's not a nested module (preventing conflicts)

--- src/package.cmd.js
+++ src/package.cmd.js
@@ -132,13 +132,7 @@ class PackageCommand extends AbstractCommand {
 
       // add modules that cause problems when embeded in webpack
       Object.keys(info.externals).forEach((mod) => {
-        // if a package (like request) brings its own node_modules directory, override it
-        // so that conflicts with newer versions in the upper-level node_modules are avoided
-        if (mod === 'ajv') {
-          archive.directory(info.externals[mod].replace(/\/node_modules\/request\/node_modules\//, '/node_modules/'), `node_modules/${mod}`);
-        } else {
-          archive.directory(info.externals[mod], `node_modules/${mod}`);
-        }
+        archive.directory(info.externals[mod], `node_modules/${mod}`);
         ticks[`node_modules/${mod}/package.json`] = true;
       });
 
