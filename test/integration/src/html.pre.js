6b6f7bab7ab61bdf55fb8d5b00dddf1966128b30
Lars Trieloff <trieloff@adobe.com>
2019-03-19 14:21:13 +0000

    Tests to illustrate the new pipeline extensions. Following scenarios are covered:
    - generating fake content for 404 errors (based on pathname). This could also be used for template dispatching
    - parsing two flavors of custom markdown (from https://github.com/adobe/developer.adobe.com-planning/issues/148 and Jekyll)

--- test/integration/src/html.pre.js
+++ test/integration/src/html.pre.js
@@ -10,7 +10,8 @@
  * governing permissions and limitations under the License.
  */
 const fs = require('fs');
-const _ = require('lodash');
+const _ = require('lodash/fp');
+const map = require('unist-util-map');
 const { utils } = require('./helper.js');
 const { utils2 } = require('./utils/another_helper.js');
 const { utils3 } = require('./third_helper');
@@ -29,6 +30,80 @@ module.exports.before = {
         content: {
           body: 'This file does not exist.'
         }
+      }
+    }
+  }
+}
+
+/**
+ * Finds MDAST objects that look like this:
+ * ```markdown
+ * > [!NOTE]
+ * > Content of the note
+ * ```
+ * and flags them.
+ * @param {object} node an MDAST node
+ */
+function findnotes(node) {
+  if (node && 
+    node.type === 'blockquote' && 
+    node.children && 
+    node.children[0] && 
+    node.children[0].children && 
+    node.children[0].children[0] && 
+    node.children[0].children[0].type === 'linkReference' &&
+    node.children[0].children[0].identifier && 
+    node.children[0].children[0].identifier.match(/![a-z]+/)) {
+    // get the class name
+    const classname = node.children[0].children[0].identifier.replace(/!/, 'admonition-');
+    const title = node.children[0].children[0].identifier.replace(/!/, '');
+
+    // overwrite the linkReference node
+    node.children[0].children[0] = {
+      type: "strong",
+      children: [ { type:"text", value: title}]
+    };
+
+    
+
+    // make sure the correct HTML gets generated
+    node.data = {
+      hName: 'div',
+      hProperties: {
+        'class': classname
+      }
+    };
+    
+    return node;
+  }
+  return node;
+}
+
+/**
+ * Finds "Liquid Tags" as defined by Jekyll and turns them into embeds using a (ficticious)
+ * embeds.project-helix.io service that could interpret them.
+ * @param {*} node 
+ */
+function findliquid(node) {
+  const re = /\{% (\w+) (.*) %\}/;
+
+  if (node && node.type === 'paragraph' && node.children && node.children[0] && node.children[0].type === 'text' && node.children[0].value.match(re)) {
+    const [_, tag, params] = node.children[0].value.match(re);
+    node.children[0].type = "embed";
+    node.children[0].url = `https://embeds.project-helix.io/${tag}?params=${encodeURIComponent(params)}`;
+  }
+  return node;
+}
+
+module.exports.after = {
+  parse: (payload) => {
+    const mdast = payload.content.mdast;
+
+    return {
+      content: {
+        // we want to do two things here, so we run the compose function instead of
+        // having one mega-function. map is unist-util-map and quite cool.
+        mdast: map(mdast, _.compose(findnotes, findliquid))
       }
     }
   }
