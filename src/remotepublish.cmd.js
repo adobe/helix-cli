7431a24bb450e386e7fc07c57c253b6f1f6c3552
Lars Trieloff <trieloff@adobe.com>
2020-01-06 10:41:07 +0000

    feat(publish): use new v3 publish service with dynamic defaults
    BREAKING CHANGE: the new version has support for dynamic defaults
    
    see adobe/helix-publish#264

--- src/remotepublish.cmd.js
+++ src/remotepublish.cmd.js
@@ -35,7 +35,7 @@ class RemotePublishCommand extends AbstractCommand {
     this._debug_key = null;
     this._fastly_auth = null;
     this._dryRun = false;
-    this._publishAPI = 'https://adobeioruntime.net/api/v1/web/helix/helix-services/publish@v2';
+    this._publishAPI = 'https://adobeioruntime.net/api/v1/web/helix/helix-services/publish@v3';
     this._githubToken = '';
     this._updateBotConfig = false;
     this._configPurgeAPI = 'https://app.project-helix.io/config/purge';
