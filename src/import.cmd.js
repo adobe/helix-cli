652b30efbdd244acbfc3023fb609ee2fa9a42bee
Alexandre Capt <acapt@adobe.com>
2022-04-13 09:21:39 +0200

    feat: hlx import

---
+++ src/import.cmd.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import fse from 'fs-extra';
+import opn from 'open';
+import chalk from 'chalk-template';
+import HelixImportProject from './server/HelixImportProject.js';
+import pkgJson from './package.cjs';
+import AbstractCommand from './abstract.cmd.js';
+
+export default class ImportCommand extends AbstractCommand {
+  constructor(logger) {
+    super(logger);
+    this._httpPort = -1;
+    this._open = '/tools/importer/helix-webui-importer/index.html';
+    this._cache = null;
+  }
+
+  withHttpPort(p) {
+    this._httpPort = p;
+    return this;
+  }
+
+  withOpen(o) {
+    this._open = o === 'false' ? false : o;
+    return this;
+  }
+
+  withCache(value) {
+    this._cache = value;
+    return this;
+  }
+
+  withKill(value) {
+    this._kill = value;
+    return this;
+  }
+
+  async stop() {
+    if (this._project) {
+      try {
+        await this._project.stop();
+      } catch (e) {
+        // ignore
+      }
+      this._project = null;
+    }
+    this.log.info('Helix project stopped.');
+    this.emit('stopped', this);
+  }
+
+  async setup() {
+    await super.init();
+
+    // init dev default file params
+    this._project = new HelixImportProject()
+      .withCwd(this.directory)
+      .withLogger(this._logger)
+      .withKill(this._kill);
+
+    this.log.info(chalk`{yellow     __ __    ___       ___                  }`);
+    this.log.info(chalk`{yellow    / // /__ / (_)_ __ / _ \\___ ____ ____ ___}`);
+    this.log.info(chalk`{yellow   / _  / -_) / /\\ \\ // ___/ _ \`/ _ \`/ -_|_-<}`);
+    this.log.info(chalk`{yellow  /_//_/\\__/_/_//_\\_\\/_/   \\_,_/\\_, /\\__/___/}`);
+    this.log.info(chalk`{yellow                               /___/ v${pkgJson.version}} - import mode`);
+    this.log.info('');
+
+    if (this._cache) {
+      await fse.ensureDir(this._cache);
+      this._project.withCacheDirectory(this._cache);
+    }
+
+    if (this._httpPort >= 0) {
+      this._project.withHttpPort(this._httpPort);
+    }
+
+    try {
+      await this._project.init();
+    } catch (e) {
+      throw Error(`Unable to start helix: ${e.message}`);
+    }
+  }
+
+  async run() {
+    await this.setup();
+    await this._project.start();
+    this.emit('started', this);
+    if (this._open) {
+      const url = this._open.startsWith('/')
+        ? `http://localhost:${this._project.server.port}${this._open}`
+        : this._open;
+      opn(url, { url: true });
+    }
+  }
+}
---
+++ src/server/HelixImportServer.js
@@ -0,0 +1,289 @@
+/*
+ * Copyright 2018 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+import { promisify } from 'util';
+import EventEmitter from 'events';
+import path from 'path';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import { PassThrough } from 'stream';
+import { fetch } from '../fetch-utils.js';
+import utils from './utils.js';
+import packageJson from '../package.cjs';
+import RequestContext from './RequestContext.js';
+
+const DEFAULT_PORT = 3001;
+
+/**
+ * Wraps the route middleware so it can catch potential promise rejections
+ * during the async invocation.
+ *
+ * @param {ExpressMiddleware} fn an extended express middleware function
+ * @returns {ExpressMiddleware} an express middleware function.
+ */
+function asyncHandler(fn) {
+  return (req, res, next) => (Promise.resolve(fn(req, res, next)).catch(next));
+}
+
+export default class HelixServer extends EventEmitter {
+  /**
+   * Creates a new HelixServer for the given project.
+   * @param {HelixProject} project
+   */
+  constructor(project) {
+    super();
+    this._project = project;
+    this._app = express();
+    this._port = DEFAULT_PORT;
+    this._server = null;
+    this._kill = project.kill;
+  }
+
+  /**
+   * Returns the logger.
+   * @returns {Logger} the logger.
+   */
+  get log() {
+    return this._project.log;
+  }
+
+  /**
+   * Proxy Mode route handler
+   * @param {Express.Request} req request
+   * @param {Express.Response} res response
+   */
+  async handleToolsRequest(req, res) {
+    const sendFile = promisify(res.sendFile).bind(res);
+    const ctx = new RequestContext(req, this._project);
+    const { log } = this;
+
+    // try to serve static
+    try {
+      const filePath = path.join(this._project.directory, ctx.path);
+      log.debug('trying to serve local file', filePath);
+      await sendFile(filePath, {
+        dotfiles: 'allow',
+        headers: {
+          'access-control-allow-origin': '*',
+        },
+      });
+      return;
+    } catch (e) {
+      log.debug(`Error while delivering resource ${ctx.path} - ${e.stack || e}`);
+    }
+
+    res.status(404).send(`Unknwon path: ${ctx.path}`);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  _makeProxyURL(reqUrl, base) {
+    const url = new URL(reqUrl, base);
+    url.searchParams.delete('host');
+    return url.toString();
+  }
+
+  async _doProxyRequest(ctx, url, host, req, res) {
+    ctx.log.debug(`Proxy ${req.method} request to ${url}`);
+
+    if (this._project.cacheDirectory) {
+      const cached = await utils.getFromCache(
+        ctx.path,
+        ctx.queryString,
+        this._project.cacheDirectory,
+        ctx.log,
+      );
+      if (cached) {
+        res
+          .status(cached.status)
+          .set(cached.headers)
+          .send(cached.body);
+        return;
+      }
+    }
+
+    let body;
+    // GET and HEAD requests can't have a body
+    if (!['GET', 'HEAD'].includes(req.method)) {
+      body = new PassThrough();
+      req.pipe(body);
+    }
+    const stream = new PassThrough();
+    req.pipe(stream);
+    const headers = {
+      ...req.headers,
+    };
+    // delete headers.cookie;
+    delete headers.connection;
+    delete headers.host;
+
+    let proxyUrl = url;
+    if (ctx.queryString) {
+      proxyUrl = `${url}${ctx.queryString}`;
+    }
+    const ret = await fetch(proxyUrl, {
+      method: req.method,
+      headers,
+      cache: 'no-store',
+      body,
+      redirect: 'manual',
+    });
+
+    const contentType = ret.headers.get('content-type') || 'text/plain';
+    const level = utils.status2level(ret.status, true);
+    ctx.log[level](`Proxy ${req.method} request to ${url}: ${ret.status} (${contentType})`);
+
+    // because fetch decodes the response, we need to reset content encoding and length
+    const respHeaders = ret.headers.plain();
+    delete respHeaders['content-encoding'];
+    delete respHeaders['content-length'];
+    delete respHeaders.location;
+    respHeaders['access-control-allow-origin'] = '*';
+
+    if (this._project.cacheDirectory) {
+      const buffer = await ret.buffer();
+      await utils.writeToCache(
+        ctx.path,
+        ctx.queryString,
+        this._project.cacheDirectory,
+        {
+          body: buffer,
+          headers: respHeaders,
+          status: ret.status,
+        },
+        ctx.log,
+      );
+      res
+        .status(ret.status)
+        .set(respHeaders)
+        .send(buffer);
+      return;
+    }
+
+    if (contentType.includes('html') || contentType.includes('text')) {
+      // make urls relative
+      let text = await ret.text();
+      text = text.replace(host, '/');
+      res
+        .status(ret.status)
+        .set(respHeaders)
+        .send(text);
+      ret.body.pipe(res);
+      return;
+    }
+
+    res
+      .status(ret.status)
+      .set(respHeaders);
+    ret.body.pipe(res);
+  }
+
+  /**
+   * Proxy Mode route handler
+   * @param {Express.Request} req request
+   * @param {Express.Response} res response
+   */
+  async handleProxyModeRequest(req, res) {
+    const ctx = new RequestContext(req, this._project);
+    const { log } = this;
+
+    let host = ctx.params?.host;
+    if (!host) {
+      // first call sets the cookie, next calls use the cookie
+      host = req.cookies['hlx-proxyhost'];
+    }
+
+    if (!host) {
+      res.status(403).send('Missing host parameter');
+    } else {
+      try {
+        res.cookie('hlx-proxyhost', host);
+        const url = this._makeProxyURL(ctx.url, host);
+        await this._doProxyRequest(ctx, url, host, req, res);
+      } catch (err) {
+        log.error(`Failed to proxy helix request ${ctx.path}: ${err.message}`);
+        res.status(502).send(`Failed to proxy helix request: ${err.message}`);
+      }
+    }
+
+    this.emit('request', req, res, ctx);
+  }
+
+  async setupApp() {
+    this._app.get('/.kill', (req, res) => {
+      res.send('Goodbye!');
+      this.stop();
+    });
+
+    this._app.get('/tools/*', asyncHandler(this.handleToolsRequest.bind(this)));
+    this._app.get('*', asyncHandler(this.handleProxyModeRequest.bind(this)));
+  }
+
+  withPort(port) {
+    this._port = port;
+    return this;
+  }
+
+  isStarted() {
+    return this._server !== null;
+  }
+
+  get port() {
+    return this._port;
+  }
+
+  async start() {
+    const { log } = this;
+    if (this._port !== 0) {
+      if (this._kill && await utils.checkPortInUse(this._port)) {
+        await fetch(`http://localhost:${this._port}/.kill`);
+      }
+      const inUse = await utils.checkPortInUse(this._port);
+      if (inUse) {
+        throw new Error(`Port ${this._port} already in use by another process.`);
+      }
+    }
+    log.info(`Starting helix-simulator v${packageJson.version}`);
+    await new Promise((resolve, reject) => {
+      this._app.use(cookieParser());
+      this._server = this._app.listen(this._port, (err) => {
+        if (err) {
+          reject(new Error(`Error while starting http server: ${err}`));
+        }
+        this._port = this._server.address().port;
+        log.info(`Local Helix Dev server up and running: http://localhost:${this._port}/`);
+        if (this._project.proxyUrl) {
+          log.info(`Enabled reverse proxy to ${this._project.proxyUrl}`);
+        }
+        resolve();
+      });
+    });
+
+    await this.setupApp();
+  }
+
+  async stop() {
+    const { log } = this;
+    if (!this._server) {
+      log.warn('server not started.');
+      return true;
+    }
+    return new Promise((resolve, reject) => {
+      this._server.close((err) => {
+        if (err) {
+          reject(new Error(`Error while stopping http server: ${err}`));
+        }
+        log.info('Local Helix Dev server stopped.');
+        this._server = null;
+        resolve();
+      });
+    });
+  }
+}
