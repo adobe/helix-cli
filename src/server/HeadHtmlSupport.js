73a9429816947b4b785f6b90f0349ff0f05a03c5
Tobias Bocanegra <tripodsan@users.noreply.github.com>
2022-08-08 15:26:07 +0200

    fix: local head.html doesn't work with v7 (#2049)
    fixes #2048

--- src/server/HeadHtmlSupport.js
+++ src/server/HeadHtmlSupport.js
@@ -10,13 +10,74 @@
  * governing permissions and limitations under the License.
  */
 import fs from 'fs/promises';
+import { createHash } from 'crypto';
 import { resolve } from 'path';
-import { JSDOM } from 'jsdom';
+import { unified } from 'unified';
+import rehypeParse from 'rehype-parse';
+import { select } from 'hast-util-select';
 import { fetch } from '../fetch-utils.js';
 
 export default class HeadHtmlSupport {
+  /**
+   * prepares the dom tree for easy comparison. it hashes the nodes and stored them in
+   * a `hash` property. It also removes all whitespace-empty text nodes.
+   *
+   * @param tree
+   * @returns {string}
+   */
+  static hash(tree) {
+    const h = createHash('sha1');
+
+    const update = (obj, keys) => {
+      keys.sort();
+      for (const k of keys) {
+        let v = obj[k];
+        if (v !== undefined) {
+          if (Array.isArray(v)) {
+            v = JSON.stringify(v);
+          }
+          h.update(v);
+        }
+      }
+    };
+
+    update(tree, ['type', 'tagName', 'value']);
+    if (tree.properties) {
+      update(tree.properties, Object.keys(tree.properties));
+    }
+
+    if (tree.children) {
+      for (let i = 0; i < tree.children.length; i += 1) {
+        const child = tree.children[i];
+        // remove empty text nodes
+        if (child.type === 'text' && child.value.trim() === '') {
+          tree.children.splice(i, 1);
+          i -= 1;
+        } else {
+          h.update(HeadHtmlSupport.hash(child));
+        }
+      }
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    tree.hash = h.digest('base64');
+    return tree.hash;
+  }
+
+  /**
+   * Parses the html and returns the dom
+   * @param {string} html
+   * @returns {Promise<HastNode>}
+   */
+  static async toDom(html) {
+    return unified()
+      .use(rehypeParse, { fragment: true })
+      .parse(html);
+  }
+
   constructor({ proxyUrl, directory, log }) {
     this.remoteHtml = '';
+    this.remoteDom = null;
     this.remoteStatus = 0;
     this.localHtml = '';
     this.localStatus = 0;
@@ -30,9 +91,11 @@ export default class HeadHtmlSupport {
     const resp = await fetch(this.url, {
       cache: 'no-store',
     });
-    this.remoteHtml = this.sanitize(await resp.text());
     this.remoteStatus = resp.status;
     if (resp.ok) {
+      this.remoteHtml = (await resp.text()).trim();
+      this.remoteDom = await HeadHtmlSupport.toDom(this.remoteHtml);
+      HeadHtmlSupport.hash(this.remoteDom);
       this.log.debug('loaded remote head.html from from', this.url);
     } else {
       this.log.error(`error while loading head.html from ${this.url}: ${resp.status}`);
@@ -41,24 +104,13 @@ export default class HeadHtmlSupport {
 
   async loadLocal() {
     try {
-      this.localHtml = this.sanitize(await fs.readFile(this.filePath, 'utf-8'));
+      this.localHtml = (await fs.readFile(this.filePath, 'utf-8')).trim();
       this.localStatus = 200;
       this.log.debug('loaded local head.html from from', this.filePath);
     } catch (e) {
       this.log.error(`error while loading local head.html from ${this.filePath}: ${e.code}`);
       this.localStatus = 404;
     }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  sanitize(html) {
-    // do the same as the pipeline-service does when injecting the head.html
-    const dom = new JSDOM('<html><head></head></html>');
-    const doc = dom.window.document;
-    const $headHtml = doc.createElement('template');
-    $headHtml.innerHTML = html;
-    doc.head.appendChild($headHtml.content);
-    return doc.head.innerHTML.trim();
   }
 
   async init() {
@@ -73,31 +125,55 @@ export default class HeadHtmlSupport {
       && this.localHtml !== this.remoteHtml;
   }
 
-  replace(source) {
+  async replace(source) {
     if (!this.isModified) {
       this.log.trace('head.html ignored: not modified locally.');
       return source;
     }
 
-    // only search the head withing `<head>` in order to avoid false replacements
-    const start = source.match(/<head>/i);
-    const end = source.match(/<\/head>/i);
-    if (!start || !end) {
+    const $html = await unified()
+      .use(rehypeParse)
+      .parse(source);
+
+    const $head = select('head', $html);
+    if (!$head) {
       this.log.trace('head.html ignored: source html has no matching <head>...</head> pair.');
       return source;
     }
 
-    const idx = source.lastIndexOf(this.remoteHtml, end.index);
-    if (idx < 0) {
+    const $dst = this.remoteDom;
+    if (!$dst) {
+      // inject local content and the end of the head
+      const $last = $head.children[$head.children.length - 1];
+      const to = $last.position.end.offset;
+      return `${source.substring(0, to)}${this.localHtml}${source.substring(to)}`;
+    }
+
+    // find remote head elements in source head
+    HeadHtmlSupport.hash($head);
+    const srcLen = $head.children.length;
+    const dstLen = $dst.children.length;
+
+    let $first;
+    let $last;
+    for (let s = 0; !$last && s <= srcLen - dstLen; s += 1) {
+      $first = $head.children[s];
+      for (let d = 0; d < dstLen; d += 1) {
+        $last = $head.children[s + d];
+        if ($last.hash !== $dst.children[d].hash) {
+          $last = null;
+          break;
+        }
+      }
+    }
+
+    if (!$last) {
       this.log.debug('head.html ignored: remote not found in HTML.');
       return source;
     }
 
-    if (idx < start.index || idx > end.index) {
-      this.log.debug('head.html ignored: match outside <head>...</head> pair.', start.index, idx, end.index);
-      return source;
-    }
-
-    return `${source.substring(0, idx)}${this.localHtml}${source.substring(idx + this.remoteHtml.length)}`;
+    const from = $first.position.start.offset;
+    const to = $last.position.end.offset;
+    return `${source.substring(0, from)}${this.localHtml}${source.substring(to)}`;
   }
 }
