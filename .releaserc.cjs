699740a1c3a93f7e14dd462999b54bc056c77322
Lars Trieloff <lars@trieloff.net>
2023-11-22 08:20:24 +0100

    feat(release): announce releases on discord

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -10,7 +10,8 @@ module.exports = {
       "assets": ["package.json", "package-lock.json", "CHANGELOG.md"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    ["@semantic-release/github", {}]
+    ["@semantic-release/github", {}],
+    ["@semantic-release-discord-bot", {}]
   ],
   branches: ['main', '14.x'],
 };
