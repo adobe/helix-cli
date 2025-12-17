ae722b43acfe9384d88fbaa73b212d8e2061a28a
Tobias Bocanegra <tripod@adobe.com>
2025-12-17 11:57:20 +0100

    fix: use correct package version
    fixes #2654

--- src/cli.js
+++ src/cli.js
@@ -14,6 +14,7 @@ import camelcase from 'camelcase';
 import path from 'path';
 import chalk from 'chalk-template';
 import { resetContext } from './fetch-utils.js';
+import pkgJson from './package.cjs';
 
 const MIN_MSG = 'You need at least one command.';
 
@@ -124,6 +125,7 @@ export default class CLI {
     await logArgs(argv)
       .strictCommands(true)
       .scriptName('aem')
+      .version(pkgJson.version)
       .usage('Usage: $0 <command> [options]')
       .parserConfiguration({ 'camel-case-expansion': false })
       .env('AEM_')
